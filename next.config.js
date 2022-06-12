0e3e2b552617f30abfcbb2e33aba30c915ceae2b
Oliver White <whiteoliverg@gmail.com>
2022-06-12 16:03:26 -0400

    feat: :package: add build analyzing

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-    reactStrictMode: true,
-};
+const withPlugins = require('next-compose-plugins');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withPlugins([
+    [withBundleAnalyzer],
+    // your other plugins here
+]);
