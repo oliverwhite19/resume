8804b8c790fa25540c1b8d4f013ca9cfea4dd958
Oliver White <whiteoliverg@gmail.com>
2022-06-17 21:54:21 -0400

    fix: :bug: allow sentry configuration

--- src/pages/api/auth/[...auth0].ts
+++ src/pages/api/auth/[...auth0].ts
@@ -1,7 +1,16 @@
 import { handleAuth, handleCallback } from '@auth0/nextjs-auth0';
 import { PrismaClient } from '@prisma/client';
+import * as Sentry from '@sentry/nextjs';
+
 const prisma = new PrismaClient({
     log: ['query', 'info', 'warn', 'error'],
+});
+
+const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+
+Sentry.init({
+    dsn: SENTRY_DSN || 'https://2fd0a782eaed4eb482dfce01cbd91d56@o1291838.ingest.sentry.io/6513195',
+    tracesSampleRate: 1.0,
 });
 
 const afterCallback = async (req, res, session) => {
@@ -26,12 +35,14 @@ const afterCallback = async (req, res, session) => {
     }
 };
 
-export default handleAuth({
-    async callback(req, res) {
-        try {
-            await handleCallback(req, res, { afterCallback });
-        } catch (error) {
-            res.status(error.status || 500).end(error.message);
-        }
-    },
-});
+export default Sentry.withSentry(
+    handleAuth({
+        async callback(req, res) {
+            try {
+                await handleCallback(req, res, { afterCallback });
+            } catch (error) {
+                res.status(error.status || 500).end(error.message);
+            }
+        },
+    })
+);
