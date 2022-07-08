30dcf99f9de13c03b3a95347488838eb0ac769bf
Oliver White <whiteoliverg@gmail.com>
2022-07-08 18:36:01 -0400

    style: change the avatar image

--- src/constants/General/index.ts
+++ src/constants/General/index.ts
@@ -3,11 +3,5 @@ import { githubUrl } from '../SocialMedia';
 export const authorName = 'Oliver White';
 export const defaultUrl = githubUrl;
 
-// using both app's favicon
 export * as favicon from './favicon.ico';
-
-// using bot app's header and resume's og:image
-export * as authorImage from './author.png';
-
-// using blog og:image
-export * as defaultMetaBackground from './defaultMetaBackground.png';
+export * as authorImage from './author.jpg';
