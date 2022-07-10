0211e057afac24c5452878e8af01218e35376791
Oliver White <whiteoliverg@gmail.com>
2022-07-10 14:44:07 -0400

    feat: adjust class names for dark mode

--- public/noflash.js
+++ public/noflash.js
@@ -4,8 +4,8 @@
 (function () {
     // Change these if you use something different in your hook.
     var storageKey = 'darkMode';
-    var classNameDark = 'dark-mode';
-    var classNameLight = 'light-mode';
+    var classNameDark = 'dark-theme';
+    var classNameLight = 'light-theme';
 
     function setClassOnDocumentBody(darkMode) {
         document.body.classList.add(darkMode ? classNameDark : classNameLight);
